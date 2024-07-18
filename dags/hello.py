c078b08b998aa17f893b123c5da8b0fe8b673a5a
Yawe <gonzalo.12.98@gmail.com>
2024-07-18 22:00:35 +0200

    New DAG script for airflow

---
+++ dags/hello.py
@@ -0,0 +1,26 @@
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from datetime import datetime, timedelta
+
+default_args = {
+    'owner': 'Gonzalo',
+             'start_date': datetime(2024, 7, 18),
+             'catch_up':False
+}
+
+dag = DAG('hello_world',
+          default_args=default_args,
+          schedule=timedelta(days=1))
+
+task1 = BashOperator(
+    task_id='hello_world',
+    bash_command='echo "hello_world"'
+)
+
+task2 = BashOperator(
+    task_id='hello_gonzalo',
+    bash_command='echo "Aprende k8s, sera divertido, dijeron ******"',
+    dag=dag
+)
+
+task1 >> task2
